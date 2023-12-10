4cc4c63acc0b6f47624cff253dd0eb9ef5d3de05
lipf <lipf702@gmail.com>
2023-12-10 12:01:40 +0800

    fix: reconstruct set up openai api key

--- src/agent.py
+++ src/agent.py
@@ -14,17 +14,11 @@ from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import FAISS
 
+from src import _set_up_openai_api_key
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 LOCAL_INDEX_PATH = "../data/index"
-
-
-def set_up_openai_api_key():
-    """ 从本地文件读取openai api key设置环境变量 """
-    config_path = "../config/openai_api_key.local"
-    with open(config_path, "r") as fr:
-        key = fr.read().strip()
-    os.environ["OPENAI_API_KEY"] = key
 
 
 class QAAgent:
@@ -96,7 +90,7 @@ class QAAgent:
 
 
 def main():
-    set_up_openai_api_key()
+    _set_up_openai_api_key()
     query = "终端连不上Wi-Fi信号"
     qa_agent = QAAgent()
     qa_agent.search_engine(query)
